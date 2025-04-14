abdc111aa3d8f9e797973e3d60321ffe7da3c559
Tranche-001 <tranche100@gmail.com>
2025-04-14 14:42:27 -0300

    create createGrid and deleteGrid Function

--- script.js
+++ script.js
@@ -1,18 +1,36 @@
 const container = document.querySelector("#container")
+const btn = document.querySelector("button");
 
-
-const GRID_SIZE = 16;
-for(let i=0; i<GRID_SIZE*GRID_SIZE; i++){
-  const gridUnit = document.createElement("div");
-  gridUnit.classList.add("gridUnit");
-  gridUnit.id = "gridUnit";
-  container.appendChild(gridUnit);
-}
-const gridMatrix = document.querySelectorAll("#gridUnit")
-console.log(gridMatrix);
-gridMatrix.forEach((gridUnit) => {
-  gridUnit.addEventListener("mouseenter", (e) => {
-    console.log("AAAAAA");
-    e.target.style["background-color"] = "gray";
+function createGrid(grid_size) {
+  for(let i=0; i<grid_size*grid_size; i++){
+    const gridUnit = document.createElement("div");
+    gridUnit.classList.add("gridUnit");
+    gridUnit.id = "gridUnit";
+    container.appendChild(gridUnit);
+  }
+  const gridMatrix = document.querySelectorAll("#gridUnit")
+  console.log(gridMatrix);
+  gridMatrix.forEach((gridUnit) => {
+    gridUnit.addEventListener("mouseenter", (e) => {
+      e.target.style["background-color"] = "gray";
+    })
   })
+}
+
+function deleteGrid(){
+  const gridMatrix = document.querySelectorAll("#gridUnit");
+  gridMatrix.forEach((gridUnit) => {
+    container.removeChild(gridUnit);
+  })
+}
+
+
+
+btn.addEventListener("click", (e) => {
+  let answer = prompt("Choose a number of squares per side")
+  deleteGrid();
+  if(answer>100){
+    
+  }
+  createGrid(answer);
 })
