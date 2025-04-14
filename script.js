966943de174823d9cbfcbd6640422097f7d98fdf
Tranche-001 <tranche100@gmail.com>
2025-04-13 21:20:03 -0300

    add listener that makes the hovering work

--- script.js
+++ script.js
@@ -5,12 +5,14 @@ const GRID_SIZE = 16;
 for(let i=0; i<GRID_SIZE*GRID_SIZE; i++){
   const gridUnit = document.createElement("div");
   gridUnit.classList.add("gridUnit");
+  gridUnit.id = "gridUnit";
   container.appendChild(gridUnit);
 }
-const gridMatrix = document.querySelectorAll("gridUnit")
-
+const gridMatrix = document.querySelectorAll("#gridUnit")
+console.log(gridMatrix);
 gridMatrix.forEach((gridUnit) => {
   gridUnit.addEventListener("mouseenter", (e) => {
+    console.log("AAAAAA");
     e.target.style["background-color"] = "gray";
   })
 })
