6a451f1963029a233052d10cfe057180d877c571
Tranche-001 <tranche100@gmail.com>
2025-04-15 16:23:29 -0300

    make random color property work

--- script.js
+++ script.js
@@ -12,11 +12,20 @@ function createGrid(grid_size) {
   }
   const gridMatrix = document.querySelectorAll("#gridUnit")
   console.log(gridMatrix);
+
+  const randomHexColorCode = () => {
+    let n = (Math.random() * 0xfffff * 1000000).toString(16);
+    return '#' + n.slice(0, 6);
+  };
+  
+  
+
   gridMatrix.forEach((gridUnit) => {
     gridUnit.style.width = CONTAINER_WIDTH/grid_size + 'px';
     gridUnit.style.height = CONTAINER_WIDTH/grid_size + 'px';
     gridUnit.addEventListener("mouseenter", (e) => {
-      e.target.style["background-color"] = "gray";
+      let color = randomHexColorCode();
+      e.target.style["background-color"] = color;
     })
   })
 }
